bd4b1939417a6b4e08c8c00c7bbf43e7db29a565
Gustavo Alves <54960265+gustahmalves@users.noreply.github.com>
2021-08-25 15:25:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function criaHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR',{
+        hour12: false,
+        timeZone: 'UTC' 
+    });
+}
+
+console.log(criaHoraDosSegundos(10));
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+     timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click',function(event){
+    relogio.classList.remove('pausado');    
+    iniciaRelogio();
+});
+
+
+pausar.addEventListener('click',function(event){
+  clearInterval(timer);
+  relogio.classList.add('pausado')
+});
+
+
+zerar.addEventListener('click',function(event){
+    clearInterval(timer);
+    relogio.classList.remove('pausado');
+    segundos = 0;
+    relogio.innerHTML = '00:00:00';
+});
